190bb8a6f054fd9212636bf7e1e1aad50ff7e546
Areeb Ahmed <areebmobile@gmail.com>
2023-10-24 02:19:51 +0530

    24 A2:19

--- arrays.c
+++ arrays.c
@@ -4,11 +4,35 @@ To insert values to it, use a comma-separated list, inside curly braces:*/
 
 #include<stdio.h>
 int main() {
-    int numArray[] = {10,20,30,40,50};
-    //can not prit the elements of array directly like a variable, NEED TO USE LOOPS and print elements one by one.
+    int numArray[] = {10,20,30,40,50}; //CANNOT print the elements of array directly like a variable, NEED TO USE LOOPS and print elements one by one.
     for(int i=0; i<5 ; i++){
         printf("%d\n",numArray[i]);
-        // printf("%d ", numArray[i]); this would be horizontal representation
+    // printf("%d ", numArray[i]); this would be horizontal representation
+    }
+    
+    printf("\n\n");
+    /*INDEXING*/
+    int array[] = {1,2,3,4,5};
+    printf("%d\n", array[0]);
+    printf("%d\n", array[1]);
+    printf("%d\n", array[2]);
+    printf("%d\n", array[3]);
+    printf("%d\n", array[4]);
+
+    printf("\n\n");
+    /*SET ARRAY SIZE*/
+    int numbers[5] ;
+
+    //now adding the numbers into the array:
+    numbers[0] = 5;
+    numbers[1] = 6;
+    numbers[2] = 7;
+    numbers[3] = 8;
+    numbers[4] = 9;
+
+    //lets print and check : 
+    for(int a=0; a<5; a++){
+        printf("%d ",numbers[a]);         //prints 5 6 7 8 9 ---> PERFECT!
     }
 }
 
