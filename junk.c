8286923a09d00b1da5495c93864ac155e0426058
Areeb Ahmed <areebmobile@gmail.com>
2023-10-24 02:10:00 +0530

    zero-th

---
+++ junk.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    printf("Hello @emareeeb!\n");
+    int num = 10;
+    printf("%d", num);
+    // Explicit (manual conversion of data type:)
+    float div = (float) 5 / 2;
+    printf("%f\n", div);   //run the code without additional statements - compiler issue
+    float implicit = 4/2;
+    printf("%f\n", implicit);
+}
---
+++ loops.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main() {
+    //while loop :
+    int i = 0;
+    while(i<=10){
+        printf("%d\n",i);
+        i++;
+    }
+    printf("\n\n");
+    
+    
+    //do while loop
+    int j = 10;
+    do{
+        printf("%d\n",j);
+        j--;
+    }while(j>=0);
+
+    printf("\n\n");
+    
+    
+    //for loop : for(initialization ; condition ; incre/decrement)
+    for(int x = 0; x <= 10; x++){
+        printf("%d",x);
+    }
+    
+    printf("\n\n");
+    
+    for(int z = 9; z>=0; z--){
+        printf("%d",z);
+    }
+}
