daeaa575d7a0a7de4763fc1ac9274af1234530ca
Areeb Ahmed <132186043+emareeeb@users.noreply.github.com>
2023-11-03 17:24:22 +0530

    comment on error fixed

--- junk.c
+++ junk.c
@@ -5,7 +5,7 @@ int main() {
     printf("%d", num);
     // Explicit (manual conversion of data type:)
     float div = (float) 5 / 2;
-    printf("%f\n", div);   //run the code without additional statements - compiler issue
+    printf("%f\n", div);   //run the code without additional statements - displays 'compiler issue'
     float implicit = 4/2;
     printf("%f\n", implicit);
 }
